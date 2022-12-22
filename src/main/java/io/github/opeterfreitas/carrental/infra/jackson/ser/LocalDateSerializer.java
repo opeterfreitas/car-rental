7b273662fb553b955bd46214a08488df20f6a6d9
Peter Freitas <opeterfreitas@gmail.com>
2022-12-22 08:33:59 -0300

    Ajustes no formato de datas - Serialização e Deserialização

---
+++ src/main/java/io/github/opeterfreitas/carrental/infra/jackson/ser/LocalDateSerializer.java
@@ -0,0 +1,24 @@
+package io.github.opeterfreitas.carrental.infra.jackson.ser;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import io.github.opeterfreitas.carrental.infra.utils.JavaTimeUtils;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+	private static final long serialVersionUID = -2718386750062666481L;
+
+	public LocalDateSerializer() {
+		super(LocalDate.class);
+	}
+
+	@Override
+	public void serialize(final LocalDate value, final JsonGenerator generator, final SerializerProvider provider)
+			throws IOException {
+		generator.writeString(value.format(JavaTimeUtils.LOCAL_DATE_FORMATTER));
+	}
+}

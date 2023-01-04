8e7505014395883c57c6ae54b2331a054b7ce400
Peter Freitas <opeterfreitas@gmail.com>
2023-01-04 18:17:48 -0300

    Ajustes implementação update CarRental

--- src/main/java/io/github/opeterfreitas/carrental/model/services/impl/CarRentalServiceImpl.java
+++ src/main/java/io/github/opeterfreitas/carrental/model/services/impl/CarRentalServiceImpl.java
@@ -1,6 +1,7 @@
 package io.github.opeterfreitas.carrental.model.services.impl;
 
 import io.github.opeterfreitas.carrental.model.entities.CarRental;
+import io.github.opeterfreitas.carrental.model.entities.Invoice;
 import io.github.opeterfreitas.carrental.model.repositories.CarRentalRepository;
 import io.github.opeterfreitas.carrental.model.services.CarRentalService;
 import io.github.opeterfreitas.carrental.model.services.exceptions.ObjectNotFoundException;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
@@ -74,32 +76,35 @@ public class CarRentalServiceImpl implements CarRentalService {
         var carRentalExists = findById(id);
         CarRental carRental = dto.toModel();
         carRental.setId(carRentalExists.getId());
+        if (dto.getDateReturn() == null) {
+            processInvoice(carRental);
+            return repository.save(carRental);
+        }
         return repository.save(carRental);
     }
 
-//    public void processInvoice(CarRental carRental) {
-//
-//        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
-//        double hours = minutes / 60.0;
-//
-//        double basicPayment;
-//        if (hours <= 12.0) {
-//            basicPayment = carRental.getVehicle().getPricePerHour() * Math.ceil(hours);
-//        } else {
-//            basicPayment = carRental.getVehicle().getPricePerDay() * Math.ceil(hours / 24);
-//        }
-//
-//        double tax = tax(basicPayment);
-//
-//        carRental.setInvoice(new Invoice(basicPayment, tax));
-//    }
-//
-//    public double tax(double basicPayment) {
-//        if (basicPayment <= 100.0) {
-//            return basicPayment * 0.2;
-//        } else {
-//            return basicPayment * 0.15;
-//        }
-//    }
+    public void processInvoice(CarRental carRental) {
 
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = carRental.getVehicle().getPricePerHour() * Math.ceil(hours);
+        } else {
+            basicPayment = carRental.getVehicle().getPricePerDay() * Math.ceil(hours / 24);
+        }
+
+        double tax = tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+
+    public double tax(double basicPayment) {
+        if (basicPayment <= 100.0) {
+            return basicPayment * 0.2;
+        } else {
+            return basicPayment * 0.15;
+        }
+    }
 }
